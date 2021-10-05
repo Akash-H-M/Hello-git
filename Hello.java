60f9f64c59906ce4b3b74bda0768b065f7092c3c
Akash-H-M <akashhm2000@gmail.com>
2021-10-05 20:52:12 +0530

    adding hello file

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+	public static void main(String[] args) {
+		int i=0;
+		while(i<10)
+		{
+			if(i%2==0)
+			{
+				System.out.println("hii akash!");
+			}
+			else
+				System.out.println("welcome to git...");
+			i++;
+		}
+	}
+}
